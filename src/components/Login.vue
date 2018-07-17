a6a92f8d3ae0ce5548e32b88bf1a0eafc810fae9
colorfulZebra <colorful_zebra@139.com>
2018-07-17 16:25:03 +0800

    Vuex added

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="loginpage"></div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+
+#loginpage {
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import message from './modules/message'
+
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+export default new Vuex.Store({
+  modules: {
+    message
+  },
+  strict: debug
+})
